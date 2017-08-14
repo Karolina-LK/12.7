01a91e3e6e4fc1ec7cdea45f16a7c89ccb219ddb
Karolina Lubieniecka <lubieniecka@interia.pl>
2017-08-14 16:45:09 +0200

    added changed again and corrected app file

--- App.js
+++ App.js
@@ -1,67 +1,36 @@
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
-var myHeaders = {
-  'X-Client-Id': '2025',
-  'X-Auth-Token': 'e59ca139872b498b0ee61423ce7366fc'
-};
+// KLASA KANBAN CARD
+function Card(id, name) {
+  var self = this;
+  
+  this.id = id;
+  this.name = name || 'No name given';
+  this.element = createCard();
 
-$.ajax({
-    url: baseUrl + '/board',
-    method: 'GET',
-    success: function(response) {
-      setupColumns(response.columns);
-    }
-});
-
-$.ajaxSetup({
-  headers: myHeaders
-});
-
-function setupColumns(columns) {
-    columns.forEach(function (column) {
-      var col = new Column(column.id, column.name);
-        board.createColumn(col);
-        setupCards(col, column.cards);
+  function createCard() {
+    var card = $('<li class="card"></li>');
+    var cardDeleteBtn = $('<button class="btn-delete">x</button>');
+    var cardDescription = $('<p class="card-description"></p>');
+    
+    cardDeleteBtn.click(function(){
+      self.removeCard();
     });
-}
-
-
-function setupCards(col, cards) {
-  cards.forEach(function (card) {
-        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-      col.createCard(card);
-    })
-}
-
-/*czy mam tu wpisać funkcję createCard mimo jej istnienia w cards.js?*/
-
-/*czy mam wyrzucić funckję randomString?*/
-/*// OGÓLNA FUNKCJA
-function randomString() {
-  var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-  var str = '', i;
-  for (i = 0; i < 10; i++) {
-    str += chars[Math.floor(Math.random() * chars.length)];
+    
+    card.append(cardDeleteBtn);
+    cardDescription.text(self.name);
+    card.append(cardDescription)
+    return card;
   }
-  return str;
 }
-*/
-
-
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
-
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
-
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
-
+Card.prototype = {
+  removeCard: function() {
+    var self = this;
+      $.ajax({
+        url: baseUrl + '/card/' + self.id,
+        method: 'DELETE',
+        success: function(){
+          self.element.remove();
+        }
+      });
+  }
+}
